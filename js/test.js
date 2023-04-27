ee7df0d77c8e678f6d38dd988d3f3ba0940dbe4b
SyverGiswold <syvergiswold@gmail.com>
2023-04-27 11:27:07 +0200

    small QOL changes

--- js/test.js
+++ js/test.js
@@ -5,40 +5,82 @@ const alignRightButton = document.querySelector('#align-right');
 const fontSizeUpButton = document.querySelector('#font-size-up');
 const fontSizeDownButton = document.querySelector('#font-size-down');
 const setFontSizeButton = document.querySelector('#set-font-size-button');
-const FontSize = document.querySelector('#font-size');
+const fontSize = document.querySelector('#font-size');
+const blackOverlay = document.querySelector('#black-overlay');
+const blindMode = document.querySelector('#blind-mode');
+
+let lastSelectionRange = null;
+
+textEditor.addEventListener('mouseup', () => {
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    lastSelectionRange = selection.getRangeAt(0);
+    updateFontSizeInput();
+  }
+});
+
+textEditor.addEventListener('keydown', () => {
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    lastSelectionRange = selection.getRangeAt(0);
+    updateFontSizeInput();
+  }
+});
+
+textEditor.addEventListener('keyup', () => {
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    lastSelectionRange = selection.getRangeAt(0);
+    updateFontSizeInput();
+  }
+});
+
+let isBlindModeOn = false;
+blindMode.addEventListener('click', () => {
+  isBlindModeOn = !isBlindModeOn;
+  if (isBlindModeOn) {
+    blackOverlay.style.backgroundColor = 'black';
+  } else {
+    blackOverlay.style.backgroundColor = '';
+  }
+});
 
 alignLeftButton.addEventListener('click', () => applyAlign('left'));
 alignCenterButton.addEventListener('click', () => applyAlign('center'));
 alignRightButton.addEventListener('click', () => applyAlign('right'));
-fontSizeUpButton.addEventListener('click', () => applyFontSize(1));
-fontSizeDownButton.addEventListener('click', () => applyFontSize(-1));
-setFontSizeButton.addEventListener('click', () => setFontSize(setFontSize.value));
+fontSizeUpButton.addEventListener('click', () => changeFontSize(1));
+fontSizeDownButton.addEventListener('click', () => changeFontSize(-1));
+setFontSizeButton.addEventListener('click', () => setFontSize());
 
 function applyAlign(align) {
-  const selection = window.getSelection();
-  if (selection.toString().length !== 0) {
-    const range = selection.getRangeAt(0);
-    const lineStartNode = range.startContainer.parentNode;
+  if (lastSelectionRange !== null) {
+    const lineStartNode = lastSelectionRange.startContainer.parentNode;
     lineStartNode.style.textAlign = align;
   }
-}
+};
 
-function applyFontSize(size) {
-  const selection = window.getSelection();
-  if (selection.toString().length !== 0) {
-    const range = selection.getRangeAt(0);
-    const lineStartNode = range.startContainer.parentNode;
+function changeFontSize(size) {
+  if (lastSelectionRange !== null) {
+    const lineStartNode = lastSelectionRange.startContainer.parentNode;
     const currentFontSize = parseInt(window.getComputedStyle(lineStartNode).getPropertyValue('font-size'));
     const newFontSize = currentFontSize + size;
     lineStartNode.style.fontSize = `${newFontSize}px`;
+    updateFontSizeInput();
   }
-}
+};
 
 function setFontSize() {
-  const selection = window.getSelection();
-  if (selection.toString().length !== 0) {
-    const range = selection.getRangeAt(0);
-    const lineStartNode = range.startContainer.parentNode;
-    lineStartNode.style.fontSize = `${FontSize.value}px`;
+  if (lastSelectionRange !== null) {
+    const lineStartNode = lastSelectionRange.startContainer.parentNode;
+    lineStartNode.style.fontSize = `${fontSize.value}px`;
+    updateFontSizeInput();
   }
-}
+};
+
+function updateFontSizeInput() {
+  if (lastSelectionRange !== null) {
+    const lineStartNode = lastSelectionRange.startContainer.parentNode;
+    const currentFontSize = parseInt(window.getComputedStyle(lineStartNode).getPropertyValue('font-size'));
+    fontSize.value = currentFontSize;
+  }
+};
