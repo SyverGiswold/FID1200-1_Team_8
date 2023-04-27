8426fb8c0dd0d9e3d76ee4d41f200a7936b16ed4
SyverGiswold <syvergiswold@gmail.com>
2023-04-27 08:34:50 +0200

    simplifyed

--- js/test.js
+++ js/test.js
@@ -4,58 +4,41 @@ const alignCenterButton = document.querySelector('#align-center');
 const alignRightButton = document.querySelector('#align-right');
 const fontSizeUpButton = document.querySelector('#font-size-up');
 const fontSizeDownButton = document.querySelector('#font-size-down');
+const setFontSizeButton = document.querySelector('#set-font-size-button');
+const FontSize = document.querySelector('#font-size');
 
-alignLeftButton.addEventListener('click', applyAlignLeft);
-alignCenterButton.addEventListener('click', applyAlignCenter);
-alignRightButton.addEventListener('click', applyAlignRight);
-fontSizeUpButton.addEventListener('click', applyFontSizeUp);
-fontSizeDownButton.addEventListener('click', applyFontSizeDown);
+alignLeftButton.addEventListener('click', () => applyAlign('left'));
+alignCenterButton.addEventListener('click', () => applyAlign('center'));
+alignRightButton.addEventListener('click', () => applyAlign('right'));
+fontSizeUpButton.addEventListener('click', () => applyFontSize(1));
+fontSizeDownButton.addEventListener('click', () => applyFontSize(-1));
+setFontSizeButton.addEventListener('click', () => setFontSize(setFontSize.value));
 
-function applyAlignLeft() {
+function applyAlign(align) {
   const selection = window.getSelection();
   if (selection.toString().length !== 0) {
     const range = selection.getRangeAt(0);
     const lineStartNode = range.startContainer.parentNode;
-    lineStartNode.style.textAlign = 'left';
+    lineStartNode.style.textAlign = align;
   }
 }
 
-function applyAlignCenter() {
-  const selection = window.getSelection();
-  if (selection.toString().length !== 0) {
-    const range = selection.getRangeAt(0);
-    const lineStartNode = range.startContainer.parentNode;
-    lineStartNode.style.textAlign = 'center';
-  }
-}
-
-function applyAlignRight() {
-  const selection = window.getSelection();
-  if (selection.toString().length !== 0) {
-    const range = selection.getRangeAt(0);
-    const lineStartNode = range.startContainer.parentNode;
-    lineStartNode.style.textAlign = 'right';
-  }
-}
-
-function applyFontSizeUp() {
+function applyFontSize(size) {
   const selection = window.getSelection();
   if (selection.toString().length !== 0) {
     const range = selection.getRangeAt(0);
     const lineStartNode = range.startContainer.parentNode;
     const currentFontSize = parseInt(window.getComputedStyle(lineStartNode).getPropertyValue('font-size'));
-    const newFontSize = currentFontSize + 1;
+    const newFontSize = currentFontSize + size;
     lineStartNode.style.fontSize = `${newFontSize}px`;
   }
 }
 
-function applyFontSizeDown() {
+function setFontSize() {
   const selection = window.getSelection();
   if (selection.toString().length !== 0) {
     const range = selection.getRangeAt(0);
     const lineStartNode = range.startContainer.parentNode;
-    const currentFontSize = parseInt(window.getComputedStyle(lineStartNode).getPropertyValue('font-size'));
-    const newFontSize = currentFontSize - 1;
-    lineStartNode.style.fontSize = `${newFontSize}px`;
+    lineStartNode.style.fontSize = `${FontSize.value}px`;
   }
 }
